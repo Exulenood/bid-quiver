8fc01332c526db5a1594fe17be91233aedf89107
Exulenood <sestak-p@outlook.com>
2023-09-10 19:48:30 +0200

    disarm eslint concerning style declaration and convert App.tsx for tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
-const config = {
-  extends: ['upleveled'],
-};
 
-module.exports = config;
+module.exports = {
+  extends: ['upleveled'],
+  rules: {
+    "no-use-before-define": ["error", { "variables": true, "functions": true, "classes": true, "ignorePattern": "^styles$" }]
+    // disable the rule for "style"-variables, which are sometimes defined after the call in react native
+  },
+};
