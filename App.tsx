fa683a080ea100040cf861a2be497d550bd867bb
Exulenood <sestak-p@outlook.com>
2023-09-12 21:45:30 +0200

    uninstall not maintained native-base

--- App.tsx
+++ App.tsx
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Box, NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-
-      <NativeBaseProvider>
         <View style={styles.container}>
           <Text>Open up App.tsx to start working on your app!</Text>
           <StatusBar style="auto" />
         </View>
-        <Box>Hello world</Box>
-      </NativeBaseProvider>
   );
 }
 
